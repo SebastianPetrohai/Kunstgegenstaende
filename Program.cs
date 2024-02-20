057fad6598ff9b68192a5ace3f08290edd156c2b
Sebastian Petrohai <spetrohai@schueler.hakzell.at>
2024-02-20 11:01:52 +0100

    Kunstgegenstände hinzugefügt

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Kunstgegenstaende;
+
+Kunstgegenstand mensch = new("Mensch");
+Kunstgegenstand tisch = new("Tisch", true);
+Kunstgegenstand ball = new("Ball");
+Kunstgegenstand tischGroß = new("Tisch groß",true);
