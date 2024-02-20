1277523563aa409be4494a8de4e3432330d1f1b4
Sebastian Petrohai <spetrohai@schueler.hakzell.at>
2024-02-20 11:05:45 +0100

    add class

--- Program.cs
+++ Program.cs
@@ -2,5 +2,10 @@
 
 Kunstgegenstand mensch = new("Mensch");
 Kunstgegenstand tisch = new("Tisch", true);
+tisch.SetPreis(50);
+
 Kunstgegenstand ball = new("Ball");
+
 Kunstgegenstand tischGroß = new("Tisch groß",true);
+tischGroß.SetPreis(150);
+
